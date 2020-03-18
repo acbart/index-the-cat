aca736b18b2935347196cbf6ecaa817e9f47427e
Austin Cory Bart <acbart@vt.edu>
2020-03-18 00:34:41 -0400

    Document Level

--- src/scripts/objects/level.ts
+++ src/scripts/objects/level.ts
@@ -1,3 +1,9 @@
+/**
+ * The different types of levels, whether it asks the player to pick an Index,
+ * a Subscript, or potentially either (both). Also has the special "Win" type
+ * of level, holding the end.
+ * TODO: Have a proper separate ending Scene instead of a hacky Win level.
+ */
 export enum LevelType {
     Index,
     Subscript,
@@ -5,14 +11,37 @@ export enum LevelType {
     Win
 }
 
+/**
+ * Our levels use simple anonymous functions to generate their values, length, and indices.
+ * These type definitions formalize those functions more concretely.
+ */
+type ValueGenerator = (n: number) => String;
+// A function that consumes nothing but returns a number when executed (e.g., at random).
+type LengthGenerator = () => number;
+// A function that consumes a number and returns a list of numbers (some of which may be null)
+type IndexGenerator = (n: number) => Array<number | string | null>;
+
+/**
+ * Plain Old JavaScript Object to hold the representation of a Level.
+ */
 export class Level {
     public name: string;
     public type: LevelType;
-    public generateLength: () => number;
-    public generateValue: (number) => string;
-    public generateIndex: (number) => Array<number | null>;
+    public generateLength: LengthGenerator;
+    public generateValue: ValueGenerator;
+    public generateIndex: IndexGenerator;
 
-    constructor(name, type, generateLength, generateValue, generateIndex) {
+    /**
+     * Sets up a new level with the given name, type, and generators.
+     * @param name The name of this level.
+     * @param type The LevelType of this level.
+     * @param generateLength This function determines the length of the list for a given round.
+     * @param generateValue This function is used to generate the values within the list that we're indexing.
+     * @param generateIndex This function is used to generate the indexes the player is targetting. Can also
+     * double as a message in a pinch.
+     */
+    constructor(name: string, type: LevelType, generateLength: LengthGenerator,
+        generateValue: ValueGenerator, generateIndex: IndexGenerator) {
         this.name = name;
         this.type = type;
         this.generateValue = generateValue;
