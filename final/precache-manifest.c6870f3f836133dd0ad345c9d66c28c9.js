d130719bef38b2787fe942bf6d36a26e47172c4c
Austin Cory Bart <acbart@vt.edu>
2020-09-09 19:16:19 -0400

    Updates

---
+++ final/precache-manifest.c6870f3f836133dd0ad345c9d66c28c9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f7f960327ae90a440a99477645b7b9",
+    "url": "/index-the-cat/finalassets/audio/cat-angry.mp3"
+  },
+  {
+    "revision": "b4815e12fe94fb0c28cc91d537f3dc67",
+    "url": "/index-the-cat/finalassets/audio/cat-angry.ogg"
+  },
+  {
+    "revision": "ce2d6d59d95b88a6effd30454448157a",
+    "url": "/index-the-cat/finalassets/audio/cat-happy.mp3"
+  },
+  {
+    "revision": "34024474add44e49eadb813e8faa3543",
+    "url": "/index-the-cat/finalassets/audio/cat-happy.ogg"
+  },
+  {
+    "revision": "ef35c1db9dc5fe7a5168263f62f6ff2e",
+    "url": "/index-the-cat/finalassets/citations.md"
+  },
+  {
+    "revision": "61474e5c06da4e812a88757ce205fa3d",
+    "url": "/index-the-cat/finalassets/common_words.json"
+  },
+  {
+    "revision": "a2dc64d380902d8892ca94e8a8df5d98",
+    "url": "/index-the-cat/finalassets/common_words.txt"
+  },
+  {
+    "revision": "3510ad7af763c81732bbd066cf84d7c4",
+    "url": "/index-the-cat/finalassets/img/backdrop-thin.png"
+  },
+  {
+    "revision": "acec06498fa0ccab2d575f79edaf45b4",
+    "url": "/index-the-cat/finalassets/img/backdrop.png"
+  },
+  {
+    "revision": "6816b6e80c435da5b4fcca132882200d",
+    "url": "/index-the-cat/finalassets/img/cat/cat_head_happy.png"
+  },
+  {
+    "revision": "f28a06441a6be226bd2cef7202bee6cd",
+    "url": "/index-the-cat/finalassets/img/cat/cat_head_mad.png"
+  },
+  {
+    "revision": "9e9012bf5b666d455d426e8289916e1a",
+    "url": "/index-the-cat/finalassets/img/cat/cat_head_neutral.png"
+  },
+  {
+    "revision": "06545d97d3a58fab3d2c5bc36c8c2c9a",
+    "url": "/index-the-cat/finalassets/img/cat/cat_middle - Copy.png"
+  },
+  {
+    "revision": "5ceced400e9e91ec76b10d8d992aea65",
+    "url": "/index-the-cat/finalassets/img/cat/cat_middle.png"
+  },
+  {
+    "revision": "0d9d48b7de3ef856ad7a645785f6b30a",
+    "url": "/index-the-cat/finalassets/img/cat/cat_tail.png"
+  },
+  {
+    "revision": "c5c7151c6beb66173f0f81e6f7245bd8",
+    "url": "/index-the-cat/finalassets/img/cat/whole_cat.jpg"
+  },
+  {
+    "revision": "14c07e5ee9176931e73fc0e675e1a80a",
+    "url": "/index-the-cat/finalassets/img/cat/whole_cat.pdn"
+  },
+  {
+    "revision": "01e4938d0c0032c7c6b0f8254faf99fd",
+    "url": "/index-the-cat/finalassets/img/cat/whole_cat.png"
+  },
+  {
+    "revision": "c76415036217cc4c304b5ea0655306fd",
+    "url": "/index-the-cat/finalfavicon.ico"
+  },
+  {
+    "revision": "84ed039de233506f13d3",
+    "url": "/index-the-cat/finalgen.main.61c94c4dad035075a770.bundle.js"
+  },
+  {
+    "revision": "5d0946118a0606bd6b02",
+    "url": "/index-the-cat/finalgen.vendors.3d45cf58d45423c646a0.bundle.js"
+  },
+  {
+    "revision": "fa5faa7d51b634f3f13aa19c53ac5bfa",
+    "url": "/index-the-cat/finalicons/icons-192.png"
+  },
+  {
+    "revision": "5fc374e272918c9976e449a3d703c8be",
+    "url": "/index-the-cat/finalicons/icons-512.png"
+  },
+  {
+    "revision": "a293088d4a11b51c8c86a225d574a1df",
+    "url": "/index-the-cat/finalicons/icons8-cat-64.png"
+  },
+  {
+    "revision": "e02a1f7a368360817203405b8585bf4b",
+    "url": "/index-the-cat/finalindex.html"
+  },
+  {
+    "revision": "fecaafd0173be71f4ac68eee5f966eee",
+    "url": "/index-the-cat/finalmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/index-the-cat/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/index-the-cat/finalprecache-manifest.c6870f3f836133dd0ad345c9d66c28c9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
